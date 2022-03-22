19ae52359ed592d9f3e78e3ab8df671d7778801d
hdqtonz <hiten.qtonz@yahoo.com>
2022-03-22 17:05:01 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Difine path for express config
 const dirpath = path.join(__dirname, './public');
@@ -106,8 +107,8 @@ app.get('*', (req,res)=>{
     })
 })
 
-app.listen(3000, ()=>{
-    console.log('Server is up on prot 3000')
+app.listen(port, ()=>{
+    console.log('Server is up on prot ' + port)
 })
 
 
--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,20 +1,17 @@
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
-
 const messageOne = document.querySelector('#message_One')
 const messageTwo = document.querySelector('#message_Two')
-
-// messageOne.textContent = ''
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
 
     const loaction = search.value
+    
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + loaction).then((response) => {
+    fetch('/weather?address=' + loaction).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
